e1dd0ab3ae68421aada0878681a420de9de95026
kan-cm <kan.akihide@classmethod.jp>
2016-03-23 09:16:44 +0900

    add exercise 3.7 ~ 3.14

--- emacs/List.scala
+++ emacs/List.scala
@@ -21,13 +21,15 @@ object List {
   // 3.2
   def tail[A](xs: List[A]): List[A] = xs match {
     case Nil => Nil
-    case Cons(x, xs) => xs
+    case Cons(_, xs) => xs
+    // case Cons(x, xs) => xs
   }
 
   // 3.3
-  def setHead[A](xs: List[A], other_val: A) = xs match {
+  def setHead[A](xs: List[A], otherVal: A): List[A] = xs match {
     case Nil => Nil
-    case Cons(x, xs) => Cons(other_val, xs)
+    case Cons(_, xs) => Cons(otherVal, xs)
+    // case Cons(x, xs) => Cons(other_val, xs)
   }
 
   // 3.4 問題通りに引数「l」で定義するとパターンマッチどうやる？
@@ -43,6 +45,64 @@ object List {
     case Cons(l, ls) if !f(l) => dropWhile(ls, f)
     case Nil => ls
   }
+
+  // 3.6
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A,B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ints: List[Int]): Int = {
+    foldRight(ints, 0)(_ + _)
+  }
+
+  def product2(ds: List[Double]): Double = {
+    foldRight(ds, 1.0)(_ * _)
+  }
+
+  // 3.7
+  // できない、一度リストをすべて操作してから関数実行するため
+
+  // 3.9
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_,acc) => acc + 1)
+  }
+
+  // 3.10
+  @annotation.tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z,x))(f)
+  }
+
+  // 3.11
+  def sum3(ints: List[Int]): Int = {
+    foldLeft(ints, 0)(_ + _)
+  }
+
+  def product3(ints: List[Double]): Double = {
+    foldLeft(ints, 1.0)(_ * _)
+  }
+
+  def length2[A](as: List[A]): Int = {
+    foldLeft(as, 0)((acc,_) => acc + 1)
+  }
+
+  // 3.12
+  def reverse[A](l: List[A]): List[A] = {
+    foldLeft(l, Nil:List[A])((acc, elm) => Cons(elm, acc))
+  }
+
+  // 3.14
+  def append[A](la: List[A], lb: List[A]): List[A] =
+    foldRight(la, lb)(Cons(_,_))
+
 }
 
 // 3.1
@@ -65,3 +125,19 @@ val dropList = List.drop(List(1,2,3,4,5), 3)
 
 // 3.5
 val dropWhileList = List.dropWhile(List(1,2,3,4,5), (x: Int) => x == 3)
+
+
+// 3.8 同じListが返却される
+println(List.foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)))
+
+// 3.9
+println(List.length2(List(1,2,3,4,5)))
+
+// 3.10
+println(List.foldLeft(List(1,2,3,4,5), 0)(_ + _))
+
+// 3.12
+println(List.reverse(List(1,2,3)))
+
+// 3.14
+println(List.append(List(1,2,3), List(4,5,6)))
