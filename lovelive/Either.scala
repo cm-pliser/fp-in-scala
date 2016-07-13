5a67637bf4ce325b6338cb13fb617ddcb9e5403d
cmaraiyusuke <arai.yusuke@classmethod.jp>
2016-07-13 12:01:26 +0900

    カードがウチにそう告げるんや

--- lovelive/Either.scala
+++ lovelive/Either.scala
@@ -16,8 +16,19 @@ object either {
       case r @ \/-(_) => r
     }
 
-    def map2[AX>: A, C, D](ec: \/[AX, C])(f: (B, C) => D): \/[AX, D] =
+    def map2[AX >: A, C, D](ec: => \/[AX, C])(f: (B, C) => D): \/[AX, D] =
       flatMap { b => ec.map { c => f(b, c) } }
+
+    def tupledWith[C >: A, D](ec: C \/ D): List[C] \/ (B, D) = 
+      (this, ec) match {
+        case (-\/(a), -\/(c)) => -\/(List(a, c))
+        case (-\/(a), \/-(d)) => -\/(List(a))
+        case (\/-(b), -\/(c)) => -\/(List(c))
+        case (\/-(b), \/-(d)) => \/-(b, d)
+      }
+
+    def collectiveMap2[AX >: A, C, D](ec: => AX \/ C)(f: (B, C) => D): List[AX] \/ D =
+      tupledWith(ec).map(f.tupled)
   }
 
   final case class -\/[A](get: A) extends (A \/ Nothing)
