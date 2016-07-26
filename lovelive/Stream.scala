fa17a8747038f896691757aa81efe895cd2ff74f
cmaraiyusuke <arai.yusuke@classmethod.jp>
2016-07-26 20:37:36 +0900

    ﾎﾑﾗﾁｬﾝ

--- lovelive/Stream.scala
+++ lovelive/Stream.scala
@@ -82,6 +82,77 @@ object stream {
     def flatMap[B](f: A => Stream[B]): Stream[B] =
       foldRight(empty[B]) { (b, acc) => f(b).append(acc) }
 
+    def uMap[B](f: A => B): Stream[B] =
+      unfold(this) {
+        case Cons(h, t) => Some(f(h()), t())
+        case _ => None
+      }
+
+    def uTake(n: Int): Stream[A] =
+      unfold((n, this)) {
+        case (c, Cons(h, t)) if c > 0 => Some(h(), (c - 1, t()))
+        case _ => None
+      }
+
+    def uTakeWhile(p: A => Boolean): Stream[A] =
+      unfold(this) {
+        case Cons(h, t) if p(h()) => Some(h(), t())
+        case _ => None
+      }
+
+    def uZipWith[B, C](sb: Stream[B])(f: (A, B) => C): Stream[C] =
+      unfold((this, sb)) {
+        case (Cons(ha, ta), Cons(hb, tb)) => Some(f(ha(), hb()), (ta(), tb()))
+        case _ => None
+      }
+
+    def uZipAll[B](sb : Stream[B]): Stream[(Option[A], Option[B])] =
+      unfold((this, sb)) {
+        case (Empty, Empty) => None
+        case (Cons(ha, ta), Empty) =>
+          val v = (Some(ha()), Option.empty[B])
+          val rest = (ta(), empty[B])
+          Some(v, rest)
+        case (Empty, Cons(hb, tb)) =>
+          val v = (Option.empty[A], Some(hb()))
+          val rest = (empty[A], tb())
+          Some(v, rest)
+        case (Cons(ha, ta), Cons(hb, tb)) =>
+          val v = (Some(ha()), Some(hb()))
+          val rest = (ta(), tb())
+          Some(v, rest)
+      }
+
+    def startsWith[A](s: Stream[A]): Boolean =
+      uZipAll(s)
+        .uTakeWhile {
+          case (_, Some(_)) => true
+          case _ => false
+        }
+        .forAll {
+          case (ax, ay) => ax == ay
+        }
+
+    def tails: Stream[Stream[A]] =
+      unfold(this) {
+        case s @ Cons(_, t) => Some(s, t())
+        case _ => None
+      }.append(Stream(empty))
+
+    def hasSubsequence[A](s: Stream[A]): Boolean =
+      tails exists (_ startsWith s)
+
+    def scanRightByUnfold[B](z: => B)(f: (A, => B) => B): Stream[B] =
+      unfold(this) {
+        case Cons(h, t) => ??? /* how can we apply `f` here? */
+        case _ => z
+      }
+
+    def scanRight[B](z: => B)(f: (A, => B) => B): Stream[B] = // this signature is more like `foldRight`
+      ??? // so what?
+
+    def sTails: Stream[Stream[A]] =
+      scanRight(empty[A])(cons(_, _))
   }
 
   case object Empty extends Stream[Nothing]
@@ -105,6 +176,28 @@ object stream {
     def constant[A](a: A): Stream[A] = cons(a, constant(a)) // how about laziness or self-reference
 
     def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case Some((a, s)) => cons(a, unfold(s)(f))
+        case None => empty
+      }
+
+    val uFibs: Stream[BigInt] =
+      unfold( (BigInt(0), BigInt(1)) ) {
+        case (prev, cur) =>
+          val next = prev + cur
+          Some(prev, (cur, next))
+      }
+
+    def uFrom(n: Int): Stream[Int] =
+      unfold(n) { x => Some(x, x + 1) }
+
+    def uConstant[A](a: A): Stream[A] =
+      unfold(a)(Function.const(Some(a, a)))
+
+    val uOnes: Stream[Int] =
+      unfold(0)(Function.const(Some(1, 1))) // uConstant(1)
   }
 }
 
