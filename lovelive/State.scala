4284e9bb124b49805ebb9dd6e8bf409bf564dbde
cmaraiyusuke <arai.yusuke@classmethod.jp>
2016-08-10 15:14:30 +0900

    ハッピー☆マテリアル

--- lovelive/State.scala
+++ lovelive/State.scala
@@ -46,7 +46,7 @@ object state {
       map(nonNegativeInt)(i => i - i % 2)
 
     val doubleA: Rand[Double] =
-      map(nonNegativeInt)(i => i.toDouble / Int.MaxValue) 
+      map(nonNegativeInt)(_.toDouble / (Int.MaxValue + 1))
 
     def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
       rng => {
@@ -72,5 +72,16 @@ object state {
       val intRands = List.range(0, count).map(_ => int)
       sequence(intRands)(rng)
     }
+
+    def flatten[A](rr: Rand[Rand[A]]): Rand[A] =
+      rng => {
+        val (ra, rng2) = rr(rng)
+        ra(rng2)
+      }
+
+    def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+      flatten(map(f)(g))
+
+    def unit[A](a: A): Rand[A] = rng => (a, rng)
   }
 }
